4ab297936f090350f5337f64da784cd4ca8902b5
Algasami <105358814+algasami@users.noreply.github.com>
2022-11-27 15:30:51 +0800

    fix all

--- mediapipe/main.py
+++ mediapipe/main.py
@@ -1,14 +1,20 @@
 import time
 import cv2
 import mediapipe as mp
+import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
 
+currentDelta = 0
+
+
 def getFPS():
+    global currentDelta
     ctime = time.time()
-    fps = int(1/(ctime - getFPS.ptime))
+    currentDelta = ctime - getFPS.ptime
+    fps = int(1/(currentDelta))
     getFPS.ptime = ctime
     return fps
 
@@ -16,20 +22,21 @@ def getFPS():
 getFPS.ptime = time.time()
 
 
-def processLandmarks(image, landmarks):
-    centerOfMass = (0, 0)
-    for landmark in landmarks:
-        if landmark.visibility < 0.3:
-            continue
-        print(landmark)
-        centerOfMass = (centerOfMass[0] + landmark.x,
-                        centerOfMass[1] + landmark.y)
-    length = len(landmarks)
-    centerOfMass = (centerOfMass[0] / length, centerOfMass[1] / length)
-
+def processLandmarks(image, results):
+    centerOfMass = [0, 0]
+    centerOfPixel = [0, 0]
+    length = 1
+    for landmark, world_landmark in zip(results.pose_landmarks.landmark, results.pose_world_landmarks.landmark):
+        centerOfMass = np.add(
+            centerOfMass, [world_landmark.x, world_landmark.y])
+        centerOfPixel = np.add(centerOfPixel, [landmark.x, landmark.y])
+        length += 1
+    centerOfMass = np.divide(centerOfMass, length)
+    centerOfPixel = np.divide(centerOfPixel, length)
     h, w, c = image.shape
-    centerOfMass = (int(centerOfMass[0]*w), int(centerOfMass[1]*h))
-    cv2.circle(image, centerOfMass, 4, (255, 0, 0),
+    print(centerOfMass)
+    centerOfPixel = (int(centerOfPixel[0]*w), int(centerOfPixel[1]*h))
+    cv2.circle(image, centerOfPixel, 4, (255, 0, 0),
                thickness=6, lineType=8, shift=0)
     return centerOfMass
 
@@ -53,7 +60,7 @@ with mp_pose.Pose(
         if results.pose_landmarks == None:
             continue
 
-        print(processLandmarks(image, results.pose_landmarks.landmark))
+        processLandmarks(image, results)
         # Draw the pose annotation on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
